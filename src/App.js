1b2b6792d03bbd03ce9a0ee39ee0241ea847ef8c
Sridhar Katta <kattasridhar02@gmail.com>
2020-04-05 13:52:37 +0530

    removed background color

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const useAppStyles = makeStyles({
     marginBottom: "20px",
   },
   mainPage: {
-    backgroundColor: "#c789",
+    // backgroundColor: "#c789",
     // margin:"10px",
     padding: "10px",
     width: "100%",
